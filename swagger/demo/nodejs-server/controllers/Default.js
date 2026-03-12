30a2c68511eec15977a9d7e80f13764cf1146388
Dylan Van Assche <dylan.vanassche@vlaanderen.be>
2026-03-12 20:00:56 +0100

    swagger: demo: add examples and Docker

--- swagger/demo/nodejs-server/controllers/Default.js
+++ swagger/demo/nodejs-server/controllers/Default.js
@@ -4,11 +4,14 @@ var utils = require('../utils/writer.js');
 var Default = require('../service/DefaultService');
 
 module.exports.persoonGET = function persoonGET (req, res, next, xINSZ, xCorrelationId) {
+  xINSZ = req.get('X-INSZ');
+  xCorrelationId = req.get('X-Correlation-Id');
   Default.persoonGET(xINSZ, xCorrelationId)
     .then(function (response) {
       utils.writeJson(res, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      res.status(404);
+      res.send('Not Found\n');
     });
 };
